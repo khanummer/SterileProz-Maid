0ab58e1a5c39cc3992216e65593014080de754f9
Ummer Khan <khanummergit@gmail.com>
2021-04-05 16:29:40 -0700

    Adds Home.js Home.css Edits Header.js with div Content

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,7 +5,14 @@ import './Header.css';
 class Header extends Component {
     render() {
         return(
-            <div>hi</div>
+            <div>
+                <div>Sterile Proz Maid Services</div>
+                <div>Logo</div>
+                <div>FB IG TWITTER</div>
+                <div>1-(800)-500-8246 Call Today!</div>
+                <div>Get a free quote today</div>
+            </div>
+
         )
     }
 }
---
+++ src/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+
+class Home extends Component {
+    render() {
+        return(
+            <div>
+                <div>Home</div>
+            </div>
+
+        )
+    }
+}
+
+export default Home;
