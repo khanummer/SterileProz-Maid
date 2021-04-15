76790e91155f7b1504a97fc521862a2fb337f225
Ummer Khan <khanummergit@gmail.com>
2021-04-15 16:24:52 -0700

    Edits Home.css to add styling

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,8 +7,14 @@ class Header extends Component {
         return(
             <div className="Header">
                 {/* <div className="Header-1">Sterile Proz Maid Services</div> */}
-                <div className="Header-1"><img className="s-logo" alt="Logo" src="../../imgs/SPMS-Logo.jpg"/></div>
-                <div className="Header-3">FB IG TWITTER</div>
+                <div className="Header-1">
+                    <img className="s-logo" alt="Logo" src="../../imgs/SPMS-Logo.jpg"/>
+                </div>
+                <div className="Header-3"> 
+                    <img className="fb" src="../../imgs/facebook.png"/> 
+                    <img className="ig" src="../../imgs/instagram.png"/>
+                    <img className="twit" src="../../imgs/twitter.png"/>
+                </div>
                 <div className="Header-4">1-(800)-500-8246 Call Today!</div>
                 <div className="Header-5">Get a free quote today</div>
             </div>
